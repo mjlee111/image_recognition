54f1ebc3ac208a4846c40f85e552abad30d7830f
mjlee111 <menggu1234@naver.com>
2024-10-07 14:02:33 +0900

    fix: remove fixed filename

--- image_recognition/yolov8_detection/setup.py
+++ image_recognition/yolov8_detection/setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
+import glob
+import os
 
 package_name = 'yolov8_detection'
+
+model_files = glob.glob(os.path.join('model', '*'))
 
 setup(
     name=package_name,
@@ -11,7 +15,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/yolov8_detection_launch.py']),
-        ('share/' + package_name + '/model', ['model/yolov8n.pt']),
+        ('share/' + package_name + '/model', model_files), 
     ],
     install_requires=['setuptools'],
     zip_safe=True,
